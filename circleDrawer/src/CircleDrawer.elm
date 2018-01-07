ac74aa806de4b8f78913ddea0a702fb98d053b73
Joan Llenas <joan.llenas.maso@gmail.com>
2018-01-07 23:33:36 +0100

    Added support for adding Svg circles

--- circleDrawer/src/CircleDrawer.elm
+++ circleDrawer/src/CircleDrawer.elm
@@ -1,10 +1,13 @@
 module CircleDrawer exposing (..)
 
 import Html exposing (..)
-import Html.Attributes as Attrs exposing (..)
-import Html.Events exposing (..)
-import Json.Decode as Json
+import Html.Attributes exposing (..)
+import Html.Events as HtmlEvt exposing (onClick)
+import Json.Decode as Decode exposing (Decoder, at, field, int, map2)
 import Styles
+import Svg exposing (circle, svg)
+import Svg.Attributes as SvgAttr exposing (cx, cy, fill, height, id, r, stroke, width)
+import Svg.Events as SvgEvt exposing (on)
 
 
 main : Program Never Model Action
@@ -21,31 +24,27 @@ main =
 -- MODEL
 
 
-type alias Point =
-    { x : Float
-    , y : Float
+type alias Coords =
+    { offsetX : Int
+    , offsetY : Int
     }
 
 
 type alias Circle =
     { id : String
-    , x : Float
-    , y : Float
+    , x : Int
+    , y : Int
     , rad : Float
     }
-
-
-type SelectedCircleId
-    = String
-    | NoSelection
 
 
 type alias Model =
     { circles : List Circle
     , undos : List Circle
     , redos : List Circle
-    , selectedCircleId : SelectedCircleId
+    , selectedCircleId : Maybe String
     , changingDiameter : Bool
+    , lastId : Int
     }
 
 
@@ -54,8 +53,9 @@ init =
     ( { circles = []
       , undos = []
       , redos = []
-      , selectedCircleId = NoSelection
+      , selectedCircleId = Nothing
       , changingDiameter = False
+      , lastId = 1
       }
     , Cmd.none
     )
@@ -66,16 +66,28 @@ init =
 
 
 type Action
-    = CanvasClicked Point
+    = CanvasClick Coords
     | UndoClicked
     | RedoClicked
+
+
+createCircle : Coords -> String -> Circle
+createCircle coords newId =
+    Circle newId coords.offsetX coords.offsetY 50.0
 
 
 update : Action -> Model -> ( Model, Cmd Action )
 update action model =
     case action of
-        CanvasClicked point ->
-            ( { model | circles = [] }
+        CanvasClick coords ->
+            let
+                newId =
+                    model.lastId + 1
+            in
+            ( { model
+                | circles = model.circles |> (::) (createCircle coords (toString newId))
+                , lastId = newId
+              }
             , Cmd.none
             )
 
@@ -103,7 +115,62 @@ subscriptions model =
 -- VIEW
 
 
+coordsDecoder : Decoder Coords
+coordsDecoder =
+    map2 Coords
+        (field "offsetX" int)
+        (field "offsetY" int)
+
+
+createSvgCircle : Circle -> Maybe String -> Svg.Svg Action
+createSvgCircle circleData selectedCircleId =
+    let
+        fillColor =
+            case selectedCircleId of
+                Just id ->
+                    if circleData.id == id then
+                        "gray"
+                    else
+                        "white"
+
+                Nothing ->
+                    "white"
+    in
+    Svg.circle
+        [ SvgAttr.id circleData.id
+        , SvgAttr.cx (toString circleData.x)
+        , SvgAttr.cy (toString circleData.y)
+        , SvgAttr.r (toString circleData.rad)
+        , SvgAttr.stroke "black"
+        , SvgAttr.fill fillColor
+        ]
+        []
+
+
 view : Model -> Html Action
 view model =
-    div []
-        [ text "Hello" ]
+    div [ style Styles.mainContainerStyles ]
+        [ div [ style Styles.buttonContainerStyles ]
+            [ button
+                [ HtmlEvt.onClick UndoClicked
+                , disabled (List.length model.undos == 0)
+                ]
+                [ text "Undo" ]
+            , button
+                [ HtmlEvt.onClick RedoClicked
+                , disabled (List.length model.redos == 0)
+                ]
+                [ text "Redo" ]
+            ]
+        , div [ style Styles.svgContainerStyles ]
+            [ svg
+                [ SvgAttr.width "100%"
+                , SvgAttr.height "100%"
+                , SvgEvt.on "click" (Decode.map CanvasClick coordsDecoder)
+                ]
+                (List.map
+                    (\circle -> createSvgCircle circle model.selectedCircleId)
+                    model.circles
+                )
+            ]
+        ]
--- circleDrawer/src/Styles.elm
+++ circleDrawer/src/Styles.elm
@@ -3,8 +3,29 @@ module Styles exposing (..)
 
 mainContainerStyles : List ( String, String )
 mainContainerStyles =
-    [ ( "display", "flex" )
-    , ( "flex-direction", "column" )
-    , ( "width", "250px" )
-    , ( "padding", "10px" )
+    [ ( "position", "absolute" )
+    , ( "left", "0" )
+    , ( "right", "0" )
+    , ( "top", "0" )
+    , ( "bottom", "0" )
+    , ( "backgroundColor", "#fff" )
+    ]
+
+
+buttonContainerStyles : List ( String, String )
+buttonContainerStyles =
+    [ ( "top", "0" )
+    , ( "height", "50px" )
+    , ( "backgroundColor", "#ccc" )
+    , ( "display", "flex" )
+    , ( "alignItems", "center" )
+    , ( "justifyContent", "center" )
+    ]
+
+
+svgContainerStyles : List ( String, String )
+svgContainerStyles =
+    [ ( "top", "50px" )
+    , ( "bottom", "0" )
+    , ( "backgroundColor", "#666" )
     ]

ab56f0ae3698abba8c788fbf30d6667cd68659f3
Joan Llenas <joan.llenas.maso@gmail.com>
2018-01-08 23:23:36 +0100

    Diameter dialog implemented. WIP: undo / redo implementation

--- circleDrawer/src/CircleDrawer.elm
+++ circleDrawer/src/CircleDrawer.elm
@@ -1,13 +1,13 @@
 module CircleDrawer exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events as HtmlEvt exposing (onClick)
-import Json.Decode as Decode exposing (Decoder, at, field, int, map2)
-import Styles
+import Html.Attributes as HtmlAttrs exposing (..)
+import Html.Events as HtmlEvt exposing (..)
+import Json.Decode as Decode exposing (..)
+import Styles exposing (..)
 import Svg exposing (circle, svg)
-import Svg.Attributes as SvgAttr exposing (cx, cy, fill, height, id, r, stroke, width)
-import Svg.Events as SvgEvt exposing (on)
+import Svg.Attributes as SvgAttr exposing (..)
+import Svg.Events as SvgEvt exposing (..)
 
 
 main : Program Never Model Action
@@ -24,26 +24,30 @@ main =
 -- MODEL
 
 
+defaultCircleRadius : Float
+defaultCircleRadius =
+    50.0
+
+
 type alias Coords =
-    { offsetX : Int
-    , offsetY : Int
+    { offsetX : Float
+    , offsetY : Float
     }
 
 
 type alias Circle =
     { id : String
-    , x : Int
-    , y : Int
+    , x : Float
+    , y : Float
     , rad : Float
     }
 
 
 type alias Model =
     { circles : List Circle
-    , undos : List Circle
-    , redos : List Circle
-    , selectedCircleId : Maybe String
-    , changingDiameter : Bool
+    , undos : List (List Circle)
+    , redos : List (List Circle)
+    , selectedCircle : Maybe Circle
     , lastId : Int
     }
 
@@ -53,8 +57,7 @@ init =
     ( { circles = []
       , undos = []
       , redos = []
-      , selectedCircleId = Nothing
-      , changingDiameter = False
+      , selectedCircle = Nothing
       , lastId = 1
       }
     , Cmd.none
@@ -69,37 +72,140 @@ type Action
     = CanvasClick Coords
     | UndoClicked
     | RedoClicked
+    | CloseDiameterDialog
+    | DiameterChanged String
 
 
 createCircle : Coords -> String -> Circle
 createCircle coords newId =
-    Circle newId coords.offsetX coords.offsetY 50.0
+    Circle newId coords.offsetX coords.offsetY defaultCircleRadius
+
+
+distanceBetween : ( Float, Float ) -> ( Float, Float ) -> Float
+distanceBetween ( x1, y1 ) ( x2, y2 ) =
+    sqrt (((x1 - x2) ^ 2) + ((y1 - y2) ^ 2))
+
+
+findFirstCircleUnderPoint : List Circle -> Coords -> Maybe Circle
+findFirstCircleUnderPoint circles coords =
+    List.filter (\c -> distanceBetween ( c.x, c.y ) ( coords.offsetX, coords.offsetY ) <= c.rad) circles
+        |> List.head
 
 
 update : Action -> Model -> ( Model, Cmd Action )
 update action model =
     case action of
         CanvasClick coords ->
+            case findFirstCircleUnderPoint model.circles coords of
+                Just circle ->
+                    ( { model
+                        | selectedCircle = Just circle
+                      }
+                    , Cmd.none
+                    )
+
+                Nothing ->
+                    let
+                        newId =
+                            model.lastId + 1
+
+                        newCircles =
+                            List.append model.circles [ createCircle coords (toString newId) ]
+                    in
+                    ( { model
+                        | circles = newCircles
+                        , undos = List.append model.undos [ model.circles ]
+                        , lastId = newId
+                      }
+                    , Cmd.none
+                    )
+
+        UndoClicked ->
             let
-                newId =
-                    model.lastId + 1
+                newUndos =
+                    List.take (List.length model.undos - 1) model.undos
+
+                newCircles =
+                    List.head (List.drop (List.length model.undos - 1) model.undos)
             in
             ( { model
-                | circles = model.circles |> (::) (createCircle coords (toString newId))
-                , lastId = newId
+                | circles =
+                    case newCircles of
+                        Nothing ->
+                            model.circles
+
+                        Just circles ->
+                            circles
+                , undos = newUndos
+                , redos = model.circles :: model.redos
               }
             , Cmd.none
             )
 
-        UndoClicked ->
-            ( { model | undos = [] }
+        RedoClicked ->
+            let
+                newRedos =
+                    List.drop 1 model.redos
+
+                newCircles =
+                    List.head model.redos
+            in
+            ( { model
+                | circles =
+                    case newCircles of
+                        Nothing ->
+                            model.circles
+
+                        Just circles ->
+                            circles
+                , undos =
+                    case newCircles of
+                        Nothing ->
+                            model.undos
+
+                        Just circles ->
+                            List.append model.undos [ circles ]
+                , redos = newRedos
+              }
             , Cmd.none
             )
 
-        RedoClicked ->
-            ( { model | redos = [] }
+        CloseDiameterDialog ->
+            ( { model
+                | selectedCircle = Nothing
+              }
             , Cmd.none
             )
+
+        DiameterChanged rad ->
+            case model.selectedCircle of
+                Just circle ->
+                    let
+                        newCircle =
+                            { circle | rad = String.toFloat rad |> Result.withDefault 25 }
+
+                        newCircles =
+                            List.map
+                                (\c ->
+                                    if c.id == circle.id then
+                                        newCircle
+                                    else
+                                        c
+                                )
+                                model.circles
+                    in
+                    ( { model
+                        | selectedCircle = Just newCircle
+                        , circles = newCircles
+                        , undos = List.append model.undos [ newCircles ]
+                      }
+                    , Cmd.none
+                    )
+
+                Nothing ->
+                    ( model
+                    , Cmd.none
+                    )
 
 
 
@@ -118,17 +224,52 @@ subscriptions model =
 coordsDecoder : Decoder Coords
 coordsDecoder =
     map2 Coords
-        (field "offsetX" int)
-        (field "offsetY" int)
+        (field "offsetX" float)
+        (field "offsetY" float)
 
 
-createSvgCircle : Circle -> Maybe String -> Svg.Svg Action
-createSvgCircle circleData selectedCircleId =
+rangeValueDecoder : Decoder Action
+rangeValueDecoder =
+    Decode.map DiameterChanged HtmlEvt.targetValue
+
+
+diameterDialog : Maybe Circle -> Html Action
+diameterDialog selectedCircle =
+    case selectedCircle of
+        Nothing ->
+            div [] []
+
+        Just circle ->
+            div [ HtmlAttrs.style Styles.modalStyles ]
+                [ div [ HtmlAttrs.style Styles.changeDiameterDialogStyles ]
+                    [ button
+                        [ HtmlAttrs.style Styles.closeModalButtonStyles
+                        , HtmlEvt.onClick CloseDiameterDialog
+                        ]
+                        [ text "X" ]
+                    , span []
+                        [ text ("Change diameter: " ++ toString (circle.rad * 2))
+                        , input
+                            [ HtmlAttrs.style [ ( "width", "'200px'" ) ]
+                            , HtmlAttrs.type_ "range"
+                            , HtmlAttrs.min "10"
+                            , HtmlAttrs.value (toString circle.rad)
+                            , HtmlAttrs.max "500"
+                            , HtmlEvt.on "change" rangeValueDecoder
+                            ]
+                            []
+                        ]
+                    ]
+                ]
+
+
+svgCircle : Circle -> Maybe Circle -> Svg.Svg Action
+svgCircle circleData selectedCircle =
     let
         fillColor =
-            case selectedCircleId of
-                Just id ->
-                    if circleData.id == id then
+            case selectedCircle of
+                Just circle ->
+                    if circleData.id == circle.id then
                         "gray"
                     else
                         "white"
@@ -149,28 +290,29 @@ createSvgCircle circleData selectedCircleId =
 
 view : Model -> Html Action
 view model =
-    div [ style Styles.mainContainerStyles ]
-        [ div [ style Styles.buttonContainerStyles ]
+    div [ HtmlAttrs.style Styles.mainContainerStyles ]
+        [ div [ HtmlAttrs.style Styles.buttonContainerStyles ]
             [ button
                 [ HtmlEvt.onClick UndoClicked
                 , disabled (List.length model.undos == 0)
                 ]
-                [ text "Undo" ]
+                [ text ("Undo (" ++ toString (List.length model.undos) ++ ")") ]
             , button
                 [ HtmlEvt.onClick RedoClicked
                 , disabled (List.length model.redos == 0)
                 ]
-                [ text "Redo" ]
+                [ text ("Redo (" ++ toString (List.length model.redos) ++ ")") ]
             ]
-        , div [ style Styles.svgContainerStyles ]
+        , div [ HtmlAttrs.style Styles.svgContainerStyles ]
             [ svg
                 [ SvgAttr.width "100%"
                 , SvgAttr.height "100%"
                 , SvgEvt.on "click" (Decode.map CanvasClick coordsDecoder)
                 ]
                 (List.map
-                    (\circle -> createSvgCircle circle model.selectedCircleId)
+                    (\circle -> svgCircle circle model.selectedCircle)
                     model.circles
                 )
             ]
+        , diameterDialog model.selectedCircle
         ]
--- circleDrawer/src/Styles.elm
+++ circleDrawer/src/Styles.elm
@@ -26,6 +26,46 @@ buttonContainerStyles =
 svgContainerStyles : List ( String, String )
 svgContainerStyles =
     [ ( "top", "50px" )
-    , ( "bottom", "0" )
+    , ( "height", "calc( 100% - 50px )" )
     , ( "backgroundColor", "#666" )
+    ]
+
+
+
+-- Modal styles
+
+
+modalStyles : List ( String, String )
+modalStyles =
+    [ ( "position", "absolute" )
+    , ( "left", "0" )
+    , ( "right", "0" )
+    , ( "top", "0" )
+    , ( "bottom", "0" )
+    , ( "backgroundColor", "#333" )
+    , ( "opacity", "0.9" )
+    ]
+
+
+closeModalButtonStyles : List ( String, String )
+closeModalButtonStyles =
+    [ ( "position", "absolute" )
+    , ( "right", "10px" )
+    , ( "top", "10px" )
+    ]
+
+
+changeDiameterDialogStyles : List ( String, String )
+changeDiameterDialogStyles =
+    [ ( "position", "absolute" )
+    , ( "left", "calc(50% - 100px)" )
+    , ( "top", "calc(50% - 50px)" )
+    , ( "width", "300px" )
+    , ( "height", "150px" )
+    , ( "borderRadius", "10px" )
+    , ( "backgroundColor", "#fff" )
+    , ( "opacity", "0.9" )
+    , ( "display", "flex" )
+    , ( "alignItems", "center" )
+    , ( "justifyContent", "center" )
     ]
